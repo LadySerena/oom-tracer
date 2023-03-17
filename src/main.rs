0855fda49c49aaea4a615ff04cb829620bd60085
LadySerena <serena.tiede+github@gmail.com>
2023-03-17 13:42:12 -0500

    feat: start adding userspace side code

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
 mod Oomkill {
     include!(concat!(env!("OUT_DIR"), "/oomkill.skel.rs"));
 }
-use std::time::Duration;
-
 use anyhow::bail;
 use anyhow::Result;
 use libbpf_rs::PerfBufferBuilder;
 use plain::Plain;
+use std::time::Duration;
+use time::macros::*;
+use time::OffsetDateTime;
 use Oomkill::*;
 unsafe impl Plain for oomkill_bss_types::event {}
 
@@ -25,13 +26,22 @@ fn bump_memlock_rlimit() -> Result<()> {
 }
 
 fn handle_oom_kill(_cpu: i32, data: &[u8]) {
-    todo!()
+    let mut event = oomkill_bss_types::event::default();
+    plain::copy_from_bytes(&mut event, data).expect("data buffer too short");
 
-    // let mut oomkill = Oomkill
+    let now = if let Ok(now) = OffsetDateTime::now_local() {
+        let format = format_description!("[hour]:[minute]:[second]");
+        now.format(&format)
+            .unwrap_or_else(|_| "00:00:00".to_string())
+    } else {
+        "00:00:00".to_string()
+    };
+
+    println!("{:8} {:<7} {:<20}", now, event.pid, event.highwater_rss);
 }
 
 fn handle_lost_oom_kill(cpu: i32, count: u64) {
-    todo!()
+    eprintln!("lost {count} events on CPU {cpu}");
 }
 
 fn main() -> Result<()> {
@@ -44,13 +54,15 @@ fn main() -> Result<()> {
 
     let mut skel = open_skel.load()?;
     skel.attach()?;
-    // println!("Successfully started! Please run `sudo cat /sys/kernel/debug/tracing/trace_pipe` to see output of the BPF programs.\n");
+    println!("Successfully started! Please run `sudo cat /sys/kernel/debug/tracing/trace_pipe` to see output of the BPF programs.\n");
 
     // map is the same name as in the bpf.c file
-    let perf = PerfBufferBuilder::new(skel.maps_mut().oom_kill())
+    let perf = PerfBufferBuilder::new(skel.maps_mut().rb())
         .sample_cb(handle_oom_kill)
         .lost_cb(handle_lost_oom_kill)
         .build()?;
+
+    println!("can't get here");
 
     loop {
         perf.poll(Duration::from_millis(100))?;
